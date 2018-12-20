7145728dd183e272e767f1f6d3c6f27608932ab7
czy1996 <zzx1996@126.com>
2018-12-20 16:21:59 +0800

    app factory

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,18 @@
 from flask import Flask
 
-app = Flask(__name__)
 
+def create_app():
+    app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello World from container!'
+    @app.route('/')
+    def hello_world():
+        return 'Hello World from container!'
+
+    return app
 
 
 if __name__ == '__main__':
-    app.run(
+    create_app().run(
         host="0.0.0.0",
         port=5000,
         debug=True,
