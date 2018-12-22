f728c748de0b45ba38154a6d89547ac70e579da6
czy1996 <zzx1996@126.com>
2018-12-22 23:20:14 +0800

    todo routes

---
+++ app/routes/Todo.py
@@ -0,0 +1,15 @@
+from flask import Blueprint, current_app, jsonify
+
+from models import Todo
+
+main = Blueprint('todo', __name__)
+
+
+@main.route('/all')
+def add():
+    result = {
+        'status_code': 0,
+    }
+    todos = Todo.objects
+    result['data'] = [todo.to_json(use_db_field=False) for todo in todos]
+    return jsonify(result)
