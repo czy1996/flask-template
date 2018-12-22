8fa000cfb4040eed58d93804ee3db05d191e0948
czy1996 <zzx1996@126.com>
2018-12-22 15:53:05 +0800

    修改部署脚本 自动重启

--- reload.sh
+++ reload.sh
@@ -1,10 +1,5 @@
 # 重新构建镜像, 只构建 pyweb, mongo 一般不去管
 docker-compose -f docker-compose.yml build pyweb
 
-# 只关闭 pyweb
-docker-compose stop pyweb
-
-# 启动
-# 其实我觉得这里 mongo 也重启了，但数据应该不会丢，down 会丢失数据
-# restart always 怎么办
-docker-compose up -d
+# 这里还是根据官方文档的建议来吧
+docker-compose up --no-deps -d web
