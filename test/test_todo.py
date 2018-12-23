575742c6c6b67c383093c3230aa698ea99068c8e
czy1996 <zzx1996@126.com>
2018-12-23 17:16:23 +0800

    失败测试用例

--- test/test_todo.py
+++ test/test_todo.py
@@ -5,7 +5,12 @@ def test_all(client):
     assert 1 == r['data'][0]['id']
 
 
-def test_get_by_id(client):
+def test_get_by_id_ok(client):
     r = client.get('/todo/2').json
     assert r['data']['title'] == 'test todo2'
     assert r['data']['id'] == 2
+
+
+def test_get_by_id_none(client):
+    r = client.get('/todo/3').json
+    assert r['status_code'] == 1
