c4c3c9e14f719bd6542a1ed72585c0a715545651
czy1996 <zzx1996@126.com>
2018-12-26 20:08:23 +0800

    新增 created_at

--- app/models/Todo.py
+++ app/models/Todo.py
@@ -1,8 +1,14 @@
+import datetime
+
 from . import BaseDocument, db, BaseSchema, ma
 
 
 class TodoSchema(BaseSchema):
+    # 增必须要有点字段
     title = ma.String(required=True)
+
+    # 只导出不修改
+    created_at = ma.DateTime(dump_only=True)
 
 
 _schema = TodoSchema()
@@ -13,3 +19,4 @@ class Todo(BaseDocument):
     counter = db.SequenceField()
     title = db.StringField()
     is_deleted = db.BooleanField(default=False, required=True)
+    created_at = db.DateTimeField(default=datetime.datetime.now)
